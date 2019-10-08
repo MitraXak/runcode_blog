f6a5fe3200cc8fe4583ddf4a2213815281ff8cbb
ivan <vanechka.miron@bk.ru>
2019-10-09 00:45:10 +0300

    yes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -127,6 +127,8 @@ STATICFILES_FINDERS = [
 ]
 
 SASS_PRECISION  =  8
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'blog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #текущее расположение статических файлов
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
