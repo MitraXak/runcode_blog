fdd5643396d1541d66f7a883ec5c3871146f504e
ivan <vanechka.miron@bk.ru>
2019-10-09 01:51:19 +0300

    yes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -127,8 +127,8 @@ STATICFILES_FINDERS = [
 ]
 
 SASS_PRECISION  =  8
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join("/mitraxak.pythonanywhere.com/staticfiles") #текущее расположение статических файлов
+STATIC_URL = '/stati/'
+STATIC_ROOT = os.path.join('/static/') #текущее расположение статических файлов
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/mitraxak.pythonanywhere.com/staticfiles"),
+    os.path.join(BASE_DIR, "/static/"),
 ]
