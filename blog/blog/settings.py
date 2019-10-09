27d36e0a710146be69789adff4185cfed097e0cd
ivan <vanechka.miron@bk.ru>
2019-10-09 11:38:09 +0300

    yes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -127,5 +127,5 @@ STATICFILES_FINDERS = [
 ]
 
 SASS_PRECISION  =  8
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'blog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
