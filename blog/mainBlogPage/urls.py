59a028a8d381aa23ea92fd231a8197a9589582bb
ivan <vanechka.miron@bk.ru>
2019-10-09 09:01:42 +0300

    yes

--- blog/mainBlogPage/urls.py
+++ blog/mainBlogPage/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.static import static
 from . import views
+from ...blog import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
